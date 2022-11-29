ad40342fb7075cf61b7b6d63ae14ac139e23fc06
Daniel García Romero <skdanielg5@gmail.com>
2022-11-29 16:07:50 -0500

    [UPDATE] cors options, json encoded and url encoded. and imports

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 import express from 'express';
+import cors from 'cors';
 import { config } from './config/config.js';
+import routes from './routes/general.routes.js';
 const app = express();
+const port = config.server.port;
 
-app.get('/', (req, res) => {
-  res.send('Server in operation');
-});
+const corsOptions = {
+  origin: '*', // todo: change origin credentials for production
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use(routes);
 app.listen(config.server.port, () => {
-  console.log(`Server in port ${config.server.port}`);
+  console.log(`Server in port ${port}`);
 });
