c5a45440351f7e6128c299c186b8ac4111a984c6
Danna Paola <dannacapera16@gmail.com>
2022-12-05 00:28:56 -0500

    Se realiza el JSON de admin para poder cargar información en el frontend

--- api/admin/controller.js
+++ api/admin/controller.js
@@ -2,11 +2,23 @@ import Admin from './services.js';
 
 const admin = new Admin();
 export const getAdmin = async (req, res) => {
-  const { id } = req.params;
-  const user = await admin.findOne(id);
-  if (user.rowCount !== 0) {
-    res.status(200).json(user.rows);
-  } else {
+  try {
+    const { id } = req.params;
+    const data = await admin.findOne(id);
+    if (data.rowCount !== 0) {
+      let user = {
+        cc: data.rows[0].cc,
+        name: data.rows[0].name,
+        age: data.rows[0].age,
+        phone: data.rows[0].phone,
+        email: data.rows[0].email,
+        speciality: data.rows[0].speciality,
+        role: data.rows[0].role,
+      };
+      res.status(200).json(user);
+    }
     res.status(404).json({ message: 'User not found' });
+  } catch (error) {
+    console.error(error.message);
   }
 };
