34c3d8d1d03197a9832797eebb5f06b29cc3f4a2
Danna Paola <dannacapera16@gmail.com>
2022-12-08 22:17:37 -0500

    se cambia recovery de get a post

--- api/recovery/routes.js
+++ api/recovery/routes.js
@@ -3,6 +3,6 @@ import { getQuestion, validateAnswer } from './controller.js';
 const router = Router();
 
 router.get('/recovery/:cc', getQuestion);
-router.get('/recovery/validate/:cc', validateAnswer);
+router.post('/recovery/validate/:cc', validateAnswer);
 
 export default router;
